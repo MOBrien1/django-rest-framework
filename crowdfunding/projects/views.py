ab112adc70550b822c2d479f744a205577257567
MegsCodes <67173712+MOBrien1@users.noreply.github.com>
2020-08-22 13:17:39 +1000

    Added permissions

--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -5,6 +5,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Project, Pledge
 from .serializers import ProjectSerializer, PledgeSerializer, ProjectDetailSerializer
+from .permissions import IsOwnerOrReadOnly
 # Create your views here.
 
 class ProjectList(APIView):
@@ -28,6 +29,10 @@ class ProjectList(APIView):
         )
 
 class ProjectDetail(APIView):
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly,
+        IsOwnerOrReadOnly,
+        ]
     def get_object(self, pk):
         try:
             return Project.objects.get(pk=pk)
@@ -38,6 +43,17 @@ class ProjectDetail(APIView):
         project = self.get_object(pk)
         serializer = ProjectDetailSerializer(project)
         return Response(serializer.data)
+
+    def put(self, request, pk):
+        project = self.get_object(pk)
+        data = request.data
+        serializer = ProjectDetailSerializer(
+            instance=project,
+            data=data,
+            partial=True
+          )
+        if serializer.is_valid():
+            serializer.save()
 
 class PledgeList(APIView):
     def get(self, request):
