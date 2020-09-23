1abdbbc9af84d3091727ba090df868ba3338952a
MegsCodes <67173712+MOBrien1@users.noreply.github.com>
2020-09-23 18:52:10 +0800

    Stached changes

--- crowdfunding/crowdfunding/settings.py
+++ crowdfunding/crowdfunding/settings.py
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     'projects.apps.ProjectsConfig',
     'users.apps.UsersConfig',
     'rest_framework',
+<<<<<<< Updated upstream
     'django_filters',
+=======
+    'corsheaders',
+>>>>>>> Stashed changes
     'rest_framework.authtoken',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -58,6 +62,7 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
